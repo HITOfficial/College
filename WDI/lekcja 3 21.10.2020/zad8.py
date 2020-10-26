c5b6125cea846c329c8876a833dc655e514d4383
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2020-10-26 10:02:53 +0100

    fibonacci seq equal to number

--- WDI/lekcja 3 21.10.2020/zad8.py
+++ WDI/lekcja 3 21.10.2020/zad8.py
@@ -1,26 +1,98 @@
-# Pewnych liczb nie można przedstawić jako sumy elementów spójnych fragmentów ciągu Fibonacciego, np. 9,14,15,17,22. Proszę napisać program, który wczytuje liczbę naturalną n, wylicza i wypisuje
+# Pewnych liczb nie można przedstawić jako sumy elementów spójnych 
+# fragmentów ciągu Fibonacciego, np. 9,14,15,17,22. Proszę napisać program, 
+# który wczytuje liczbę naturalną n, wylicza i wypisuje
 # następną taką liczbę większą od n. Można założyć, że 0 < n < 1000.
 
 # Utworze elementy ciągu fibonacciego z przedziału <1,1000> i dodam je do listy
 
-def fibonacci_list(range_of_number):
-    list_of_fib_number =[]
+
+#NIEDOKOŃCZYŁEM BO SIĘ JUŻ GUBIŁEM W REKURENCJI
+
+def fibonacci_list(fib_range): # zrobiłem listę ciagu fibonacciego
+    fib_list = []
     fib1 = 1
     fib2 = 1
-    while fib2 < range_of_number:
-        list_of_fib_number.append(fib2)
-        fib1, fib2 = fib1 + fib2, fib1 # tuple w linijce pythonowo zamiast uzywac dodatkowej zmiennej
-    return list_of_fib_number
+    
+    for _ in range(fib_range):
+        fib_list.append(fib2)
+        fib1, fib2 = fib1+fib2, fib1
+    
+    return fib_list
 
-# Iteruję po 2 wymiarowej pętli, i sprawdzam czy kombinacja sumy 2 liczb fibb wynosi tej liczbie, jeśli nie to czy kombinacja n+1 również jeśli nie to printuje a jak nie to lece o 1 w górę, aż do skutku
-# wrzucę dodatkową funckję do środka w której sprawdzę, jesli podany element, +1 mozna utworzyc z 2 elementów fibb to będę sobie pobierał 
-def next_not_fibonacci_number(fib_list, number):
-    for el1 in fib_list:
-        for el2 in fib_list:
-            if  number > el1 + el2:
-                pass
 
-print(fibonacci_list(1000))
+    # mam oblecieć sobie tablice od el[1]-[n] cały czas sumując i sprawdzając czy ten ciąg da szukaną liczbę
+          # nie zadziała to lecę od el[2]-[n]
+                                #itd aż do n
+    # zrobię rekurencje, i będę się odwoływał to tej samej funckji
 
-def funct(fib1,fib2, number):
-    if fib
+def  is_a_sum_of_fib_seq(number, fib_list, start_index):   
+    suma = 0
+
+    for i in range(start_index, len(fib_list)):
+        suma += fib_list[i]
+        print(suma)
+        
+        if suma > number: 
+            start_index += 1
+            is_a_sum_of_fib_seq(number, fib_list, start_index)
+            return False
+
+
+        elif suma == number:
+            number += 1
+            print('suma jest równa')
+            is_a_sum_of_fib_seq(number, fib_list, start_index)
+            return False
+
+    if suma != number:
+        print(suma, number)
+
+
+def zad8(number):
+    fib_list = fibonacci_list(16) # do 16 elementu ciagu liczby są mniejsze od 1000
+    is_a_sum_of_fib_seq(number, fib_list, 0)
+
+
+print(zad8(18))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ # TO Źle obrobiłem bo waliłem maskę bitową
+def is_a_sum_of_fib_seq(number):
+    fib_list = fibonacci_list(13)
+
+    #spróbuję na masce bitowej obrobić zadanie
+    # 1 długość maski = 2 ^ długość tablicy - zrobię wszystkie możliwe kombinacje z elementów tablicy
+
+    for bit_mask in range(1, 2 ** len(fib_list)): # nie będe sprawdzal mozliwosci, kiedy jest 0 w masce
+        fib_combination_sum = 0
+
+        for el in fib_list:
+            if bit_mask % 2 == 0:
+                fib_combination_sum += el
+            bit_mask //= 2
+        if fib_combination_sum < 100:
+            print(fib_combination_sum)
+
+    # nie jest jakieś efektywne, bo się powtarzają liczby
+# print(is_a_sum_of_fib_seq(13))
+
+
+
+    
+
+
