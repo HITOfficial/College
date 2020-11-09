ba10810e795056dd9d3a9124fc86f8fe232049c5
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2020-11-09 11:20:21 +0100

    chess tower sum

--- WDI/lekcja 5 04.11.2020/zad20.py
+++ WDI/lekcja 5 04.11.2020/zad20.py
@@ -1,24 +1,41 @@
-# Dana jest N-elementowa tablica t zawierająca liczby naturalne mniejsze od 1000. Proszę napisać funkcję,
-#  która zwraca długość najdłuższego, spójnego fragmentu tablicy, dla którego w iloczynie jego elementów 
-# każdy czynniki pierwszy występuje co najwyżej raz. Na przykład dla tablicy t=[2,23,33,35,7,4,6,7,5,11,13,22] wynikiem jest wartość 5
+# Dana jest tablica T[N][N] (reprezentująca szachownicę) wypełniona liczbami naturalnymi.
+# Proszę napisać funkcję która ustawia na szachownicy dwie wieże, tak aby suma liczb na „szachowanych”
+# przez wieże polach była największa. Do funkcji należy przekazać tablicę, funkcja powinna zwrócić położenie
+# wież. Uwaga- zakładamy, że wieża szachuje cały wiersz i kolumnę z wyłączeniem pola na którym stoi
 
+# myslałem że zadanie jest jakieś weird, ale wychodzi na to że jest luz
 from random import randint
-from math import log10, ceil # bo przy okazji będę zaokrąglać w górę logarytm z liczby aby sprawdzić jak długa ona jest w systemie DEC
-
-def zad20(n):
-    n_list = [randint(1,1000) for _ in range(n)]
-    print(n_list)
-    # interuję po wszystkich elementach listy, i w środku dodatkowo lecę log(liczba, 10) żeby sprawdzić ile cyfr ma liczba, a potem w srodku daje dodatkową zmienną, która trzyma ilosc nieparzystych cyfr juz w tej liczbie
-
-    for el in n_list:
-        odd_counter = 0 # zmienna która mi powie, czy liczba jest parzysta
-
-        for _ in range(ceil(log10(el))):
-            if (el % 10) % 2 == 1:
-                odd_counter += 1
-            print(el % 10)
-            el //= 10
 
 
+def chess_table():
+    chess_list = [[randint(1, 100) for _ in range(8)] for _ in range(8)]
+    return chess_list
 
-zad20(5)
+
+def longest_row_col(chess_list):
+    # wstępnie zsumuję sobie każdy wiersz, i kolumnę i przypiszę je do tupla w liście (row,0,20) - (row/col, index, suma)
+    # print(chess_list)
+
+    sum_of_row_col = 16 * [0] # żeby sobie wczesniej zadeklarować długość listy, bo tak 3ba na zajęęciach to -> 0-7 rzędy, 8-15 kolumny
+
+    for i in range(len(chess_list)): # i - wiersz, j kolumna  
+        sum_of_column = 0
+        sum_of_row = 0
+        
+        for j in range(len(chess_list)):
+            sum_of_row += chess_list[i][j] # sumuje wiersze
+            sum_of_column += chess_list[j][i] # sumuje kolumny
+
+        sum_of_row_col[i] = ('ROW',i ,sum_of_row) # 0-7 będą sumy wierwszy
+        sum_of_row_col[8+i] = ('COL',i ,sum_of_column) # 8-15 sumy kolumn
+
+
+    sum_of_row_col.sort(key=lambda tup: tup[2], reverse=True)
+
+    print(sum_of_row_col[0:3])
+
+    # nie jest zrobione dokładnie bo jest łacznie z polem na którym stoi
+
+longest_row_col(chess_table())
+
+
