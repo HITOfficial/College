dab290ba04df6788b034d7dbb3a5b0f3637c0452
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2020-11-24 22:36:29 +0100

    reccurence small chess  king exercisse n+1 move

---
+++ WDI/lekcja 8 25.11.2020/zad3.py
@@ -0,0 +1,42 @@
+# Szachownica jest reprezentowana przez tablicę T[8][8] wypełnioną liczbami naturalnymi zawierającymi koszt przebywania na danym polu szachownicy. Król szachowy znajduje się w wierszu 0 i kolumnie
+# k. Król musi w dokładnie 7 ruchach dotrzeć do wiersza 7. Proszę napisać funkcję, która wyznaczy minimalny
+# koszt przejścia króla. Do funkcji należy przekazać tablicę t oraz startową kolumnę k. Koszt przebywania na
+# polu startowym i ostatnim także wliczamy do kosztu przejścia.
+
+# obskoczyć musi to rekurencyjnie
+# koszt  poruszania się to wartość pod którą stoi król
+# ilość ruchów przy każdym kolejnym się zwiększa o 1 i 7 wiersz w 7 ruchów, więc przy każdym kolejnym ruchu musi  iść i+1 a j: j-1, j, j+1 <- 3 wybory
+
+# 
+from random import randint
+
+def two_dim_list(n=8):
+    return [[randint(1,30) for _ in range(n)] for _ in range(n)]
+
+
+# nie działa to idealnie, bo algorytm kalkuluje ruch tylko o 1 wprzód
+
+def move_king(list_of_weight, actual_position, move_count, weight): # position as tuple (i,j), number of moves, weight of done moves
+    i = actual_position[0] # row
+    j = actual_position[1] # col
+    
+    moved = False
+    if j-1 > 0 and list_of_weight[i][j] > list_of_weight[i][j-1]:
+        moved = True
+        j -= 1
+
+    if moved and j+2 < len(list_of_weight) and list_of_weight[i][j] > list_of_weight[i][j+2]:
+        j += 2 # bo gdy się cofnę o jeden, to wtedy 3 wybór, to muszę inkrementować o 2
+
+    if moved == False and j+1 < len(list_of_weight) and list_of_weight[i][j] > list_of_weight[i][j+1]:
+        j += 1 # gdy nie przemieściłem w lewo to teraz sprawdzam tylko o jedno w prawą stronę
+    
+    weight += list_of_weight[i][j]
+    move_count += 1 # przygotowuję wartości do kolejnej rekurencji
+
+    if move_count < 7:
+        move_king(list_of_weight, (i+1,j), move_count, weight)
+    else:
+        return print(weight)
+
+print(move_king(two_dim_list(),(0,4), 0, 0))
