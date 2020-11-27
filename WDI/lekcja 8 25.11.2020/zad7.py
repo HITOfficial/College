cb8d55a4b7e652426858287b6a1592652c40a059
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2020-11-27 17:34:29 +0100

    weight scale pan recurrsive limit 1 weights

--- WDI/lekcja 8 25.11.2020/zad7.py
+++ WDI/lekcja 8 25.11.2020/zad7.py
@@ -13,23 +13,38 @@
 
 # *lista <- wypakowywuje liste
 
-def is_able_to_weight(weights_list, actual_weight_index, weight_to_find, actual_weight, used_weight_list=[]): # lista odważników, aktualny odważnik, waga do odważenia, aktualana waga z odważników, dodatkowo dorzuciłem kombinację odważników odmierzających daną wagę
+# unlimited weights 
+def is_able_to_weight_unlimited(weights_list, actual_weight_index, weight_to_find, actual_weight, used_weight_list=[]): # lista odważników, aktualny odważnik, waga do odważenia, aktualana waga z odważników, dodatkowo dorzuciłem kombinację odważników odmierzających daną wagę
     if actual_weight > weight_to_find:
         return # gdy znajdę za dużą wagę to zakańczam rekurencję
     if actual_weight == weight_to_find:
         return (
             print(used_weight_list),
-            print('znaleziono wagę')
         )
     else:
         if actual_weight_index < len(weights_list) -1:
-            is_able_to_weight(weights_list, actual_weight_index, weight_to_find, actual_weight + weights_list[actual_weight_index], [*used_weight_list, weights_list[actual_weight_index]]) # próbuję jeszcze raz położyć ten sam odważnik
-            is_able_to_weight(weights_list, actual_weight_index+1, weight_to_find, actual_weight + weights_list[actual_weight_index+1], [*used_weight_list, weights_list[actual_weight_index+1]]) # biorę koleny z listy odważnik jeszcze raz położyć ten sam odważnik
+            is_able_to_weight_unlimited(weights_list, actual_weight_index+1, weight_to_find, actual_weight, used_weight_list) # nie daję aktualnego odważnika
+            is_able_to_weight_unlimited(weights_list, actual_weight_index, weight_to_find, actual_weight + weights_list[actual_weight_index], [*used_weight_list, weights_list[actual_weight_index]]) # próbuję jeszcze raz położyć ten sam odważnik
+            is_able_to_weight_unlimited(weights_list, actual_weight_index+1, weight_to_find, actual_weight + weights_list[actual_weight_index+1], [*used_weight_list, weights_list[actual_weight_index+1]]) # biorę koleny z listy odważnik jeszcze raz położyć ten sam odważnik
         if actual_weight_index == len(weights_list) -1: # gdy już nie ma kolejnych odważników lecę do wyczerpania na ostatnim
-            is_able_to_weight(weights_list, actual_weight_index, weight_to_find, actual_weight + weights_list[actual_weight_index], [*used_weight_list, weights_list[actual_weight_index]])
+            is_able_to_weight_unlimited(weights_list, actual_weight_index, weight_to_find, actual_weight + weights_list[actual_weight_index], [*used_weight_list, weights_list[actual_weight_index]])
 
 
-is_able_to_weight([2,3,4],0,30,0,[])
+is_able_to_weight_unlimited(sorted([2,3,4]),-1,17,0,[])
+
+# limit 1 of every weight
+def is_able_to_weight(weight_to_find, weights_list, index_of_actual_weight=-1, actual_weight=0, used_weights_list=[]): # waga którą ma znaleźć, lista_odważników za pomocą której ma odmierzyć, aktualnie użyty odważnik, waga którą już odmierzono, użyte odważniki do obmierzenia tej wagi
+    #warunki zakończenia rekurencji
+    if actual_weight == weight_to_find:
+        return (
+            print(used_weights_list)
+        )
+    elif actual_weight > weight_to_find or index_of_actual_weight == len(weights_list)-1:
+        return
+    else:
+        if index_of_actual_weight < len(weights_list) -1: # zeby nie wyskoczyć poza tablicę odważników
+            is_able_to_weight(weight_to_find, weights_list, index_of_actual_weight +1, actual_weight + weights_list[index_of_actual_weight+1], [*used_weights_list, weights_list[index_of_actual_weight+1]])
+            is_able_to_weight(weight_to_find, weights_list, index_of_actual_weight +1, actual_weight, used_weights_list) # nie dodaję obecnego odważnika
 
 
-
+is_able_to_weight(13, [7,6,3,2,1,5])
