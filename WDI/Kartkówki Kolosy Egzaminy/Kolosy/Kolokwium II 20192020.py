7e154101067e11d7d3c4efb099bc8b80104267db
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2020-12-16 19:04:58 +0100

    combination changing number into another in N moves

--- WDI/Kartkówki Kolosy Egzaminy/Kolosy/Kolokwium II 20192020.py
+++ WDI/Kartkówki Kolosy Egzaminy/Kolosy/Kolokwium II 20192020.py
@@ -87,4 +87,82 @@ def find_equal_part(t1, t2): # t1 < t2
                 return True
 
 
-print(find_equal_part(random_2_dimention_list(7),random_2_dimention_list(13)))
+# print(find_equal_part(random_2_dimention_list(7),random_2_dimention_list(13)))
+
+
+
+# Na zbiorze liczb całkowitych określono trzy operacje: A,B,C przekształcające liczby:
+#  A: zwiększa liczbę o 3;
+#  B: podwaja liczbę;
+#  C: wszystkie parzyste cyfry w liczbie zwiększa o 1, np. 2356->3357, 1999->1999.
+# Proszę napisać funkcję która sprawdza czy można przekształcić liczbę X na liczbę Y w nie więcej niż N krokach.
+# Do funkcji należy przekazać wartości X,Y,N, funkcja powinna zwrócić liczbę możliwych ciągów operacji
+# przekształcających liczbę X w Y (lub wartość 0 jeżeli takie przekształcenie nie istnieje). Uwaga: zabronione jest
+# używanie kolejno dwóch tych samych operacji.
+
+def A(number):
+    return number + 3
+
+def B(number):
+    return number * 2
+
+def C(number):
+    tmp = number
+    i = 0
+
+    while tmp > 0:
+        i += 1
+        tmp //= 10
+
+    number_as_list  = i * [0]
+
+    while number > 0:
+        i -= 1
+        number_as_list[i] = number % 10
+        number //= 10
+    
+    for i in range(len(number_as_list)): # parzyszte zwiekszam o 1 
+        if number_as_list[i] %2 == 0:
+            number_as_list[i] += 1
+
+    power = -1
+    val_to_increment = 0
+    for el in number_as_list[::-1]:
+        power += 1 
+        if el >= 10:
+            number += val_to_increment + el%10 * (10  ** power)
+            el //= 10
+            val_to_increment = el
+        else:
+            number += val_to_increment + el%10 * (10  ** power)
+            val_to_increment = 0
+    
+    return number
+
+
+def is_able_to_create(x,y,n,last='',combination=''):
+    summary = 0
+    if n >= 0:
+        if x == y:
+            print(combination)
+            return 1
+        if last != 'A':
+            summary += is_able_to_create(A(x), y, n-1, 'A', combination+'A')
+        if last != 'B':
+            summary += is_able_to_create(B(x), y, n-1, 'B', combination+'B')
+        if last != 'C':
+            summary += is_able_to_create(C(x), y, n-1, 'C', combination+'C')
+        
+    return summary
+
+def is_posible_to_create(x,y,n):
+    return is_able_to_create(x,y,n)
+
+
+print(is_posible_to_create(11,31,4))
+
+
+
+    
+
+
