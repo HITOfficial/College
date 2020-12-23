6ce60e0a93071814b3500d2409d6f9a447bcd781
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2020-12-23 23:44:00 +0100

    Linklist adding elements to Node

--- WDI/lekcja 11 16.12.2020/zad1.py
+++ WDI/lekcja 11 16.12.2020/zad1.py
@@ -1,38 +1,53 @@
-# 1. Zaimplementuj zbiór mnogościowy liczb naturalnych korzystając ze
+# Zaimplementuj zbiór mnogościowy liczb naturalnych korzystając ze
 # struktury listy odsyłaczowej.
 # - czy element należy do zbioru
 # - wstawienie elementu do zbioru
 # - usunięcie elementu ze zbioru
 
-# zrobione na podstawie imitacji stosu
-class Node: # węzeł jednostronny
-    def __init__(self, prev_node=None, value=0):
-        self.prev_node = prev_node
+# zeby sie 3mac tendencji programowania obiektowego
+class Node:
+    def __init__(self, value=0, previous=None, next_one=None):
         self.value = value
+        self.previous = previous
+        self.next_one = next_one
 
 
-node = Node()
+def add_element(value, node): # wartosc do wstawienia, wezel
+    first_node = node # defaultowo wynikiem funkcji bede zwracal pierwszy element z wezla
+    node_previous = node # 3mam go, zeby przechowywac w nim wynik przed ostatniego przesuniecia w nodach
+    while node.next_one != None and node.value < value: # lape element przed z wezlu
+        node_previous = node
+        node = node.next_one
 
+    node = node_previous # zatrzymuje przed ostatnia iteracje z while, gdyby sie zmienila
 
-def element_in_node(node, searching_val):
-    if node.prev_node != None:
-        if node.value == searching_val:
-            return True
-        return element_in_node(node.prev_node, searching_val)
-    return False
+    if node.next_one != None and node.next_one.value == value: # mam juz taka wartosc w wezle
+        return node
+    
+    new_node = Node(value) # wezel ktory bede dodawal
 
+    if node.previous == None and node.value > value: # dodaje na poczatek
+        new_node.next_one = node
+        node.previous = new_node
+        return new_node # defaultowo bede zwracal pierwszy element ze zbioru
 
-def add_element_to_node(node, value): # dodaje na początek węzłu 
-    if node.prev_node == None:
-        node.prev_node = Node(None, value) # tworzę nowy element węzłu na początku i dodaję w nim wartość 
-    else:
-        add_element_to_node(node.prev_node, value)
+    if node.next_one == None: # dodaje na koniec wezla element
+        new_node.previous = node
+        node.next_one = new_node
+        return first_node
+    
+    new_node.previous = node # dorzucam element pomiedzy dwa wezly
+    new_node.next_one = node.next_one
+    node.next_one.previous = new_node
+    node.next_one = new_node
 
+    return first_node
+        
+            
+def show_nodes(node):
+    print(node.value)
+    while node.next_one != None:
+        print(node.next_one.value)
+        node = node.next_one
 
-def remove_first_element_from_node(node, preview= None): # usuwa element z początku węzła 
-    if node.prev_node == None:
-        preview.prev_node = None # zakańczam węzeł na przedostatnim elemencie z początku
-    else:
-        remove_first_element_from_node(node.prev_node, node)
-
-
+# show_nodes(node)
