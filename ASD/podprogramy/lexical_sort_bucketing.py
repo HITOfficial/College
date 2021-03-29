7e32ffb68819aee116f97a3cbfe3ca0481f00ceb
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2021-03-29 15:01:28 +0200

    Lexical sort using Bucet sort and  Count sort

--- ASD/podprogramy/lexical_sort_bucketing.py
+++ ASD/podprogramy/lexical_sort_bucketing.py
@@ -18,44 +18,39 @@ def count_sort(Arr,k):
     for i in range(len(Arr)):
         sorted_Arr[count_Arr[ord(Arr[i][k])-97]-1] = Arr[i]
         count_Arr[ord(Arr[i][k])-97] -= 1
-    for i in range(len(Arr)):
-        Arr[i] = sorted_Arr[i]
+    return sorted_Arr
+    # for i in range(len(Arr)):
+    #     Arr[i] = sorted_Arr[i]
 
 
-def radix_sort(Arr,n): # kubełek, długość elementów w kubełku
-    for i in range(n-1,-1,-1):
-        count_sort(Arr,i)
+# def radix_sort(Arr,n): # kubełek, długość elementów w kubełku
+#     for i in range(n-1,-1,-1):
+#         count_sort(Arr,i)
     
 
-
-def bucket_sort(Arr):
-    n = len(Arr)
-    max_len = len(Arr[0])
-    min_len = len(Arr[0])
-    for el in Arr:
-        if len(el) > max_len:
-            max_len = len(el)
-        if len(el) < min_len:
-            min_len = len(el)
-    buckets = [list() for _ in range(max_len-min_len+2)]
-    for el in Arr: # wrzucam wyrazy do odpowiedniego kubełka względem długości
-        buckets[len(el)-1].append(el)
-    for i in range(len(buckets)): # sortuje wewnątrz każdy kubełek radix_sortem
-        radix_sort(buckets[i],i+1)
-    index = 0
-    for bucket in buckets:
-        for el in bucket:
-            Arr[index] = el
-            index += 1
-    return Arr
-
+# def bucket_sort(Arr):
+#     n = len(Arr)
+#     max_len = len(Arr[0])
+#     min_len = len(Arr[0])
+#     for el in Arr:
+#         if len(el) > max_len:
+#             max_len = len(el)
+#         if len(el) < min_len:
+#             min_len = len(el)
+#     buckets = [list() for _ in range(max_len-min_len+2)]
+#     for el in Arr: # wrzucam wyrazy do odpowiedniego kubełka względem długości
+#         buckets[len(el)-1].append(el)
+#     for i in range(len(buckets)): # sortuje wewnątrz każdy kubełek radix_sortem
+#         radix_sort(buckets[i],i+1)
+#     index = 0
+#     for bucket in buckets:
+#         for el in bucket:
+#             Arr[index] = el
+#             index += 1
+#     return Arr
 
 
-
-n=10
-words = [generate_word(n) for _ in range(n)]
-print(words)
-print(bucket_sort(words))
+# print(bucket_sort(words))
 
 
 # def sort_by_letter(index, words): # index litery na podstawie ktorej bedzie sortowane słowo
@@ -98,4 +93,26 @@ print(bucket_sort(words))
 # print(lexicalsort(words))
 
 
-print(ord("a"))
+def lex_sort(Arr):
+    n = len(Arr)
+    max_len = len(Arr[0])
+    for el in Arr:
+        if len(el) > max_len:
+            max_len = len(el)
+    buckets = [list() for _ in range(max_len+1)]
+
+    for el in Arr: # wrzucam wyrazy do odpowiedniego kubełka względem długości
+        buckets[len(el)-1].append(el)
+
+    buckets[max_len-1] = count_sort(buckets[max_len-1],max_len-1) # ostatni sortuje osobno
+    for i in range(max_len-2,-1,-1):
+        buckets[i] = count_sort([*buckets[i],*buckets[i+1]],i)
+
+    for i in range(n):
+        Arr[i] = buckets[0][i]
+    return Arr
+
+n=10
+words = [generate_word(n) for _ in range(n)]
+print(words)
+print(lex_sort(words))
