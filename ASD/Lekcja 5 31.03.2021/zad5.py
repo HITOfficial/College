32dbd4af6398a46d6816d71cf7ad723bdeb6a8d1
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2021-04-11 01:30:11 +0200

    ALL LIS paths using stacks and recursion

--- ASD/Lekcja 5 31.03.2021/zad5.py
+++ ASD/Lekcja 5 31.03.2021/zad5.py
@@ -1,6 +1,30 @@
 # Rozwiazanie polega na znalezieniu wszystkich najdłuższych podciągów, i markowaniu ich w osobnej tablicy -> O(n^2)
-# liniowo szukam tych podciągów -> rozpakowywuje pierwszy znaleziony, zakończony na danym indexie, pozostałe zapamiętuje i zapisuje na stosie, który w indentyczny sposob odtwarzam 
-# Wydaje mi się że złożoność ALGO to O(n^2) -> wytlumaczylem w lini 49
+# następnie przy użyciu stosów odtwarzam najdłuższe ciągi
+A =[2,1,4,3,5,2,6]
+
+Paths_Stack = list() # w zmiennej globalnej będę zapisywał odtwarzane ścieżki
+
+def LIS(Arr,Pull,Path,index,maximum):
+    if maximum == 1: # utworzyło już wszystkie możliwe podciągi o maxymalnej długości
+        Paths_Stack.append([*Path]) # ponieważ cały czas działam na referencji do tej samej tablicy, potrzebuje kopiować ich aktualny stan
+        return 0
+
+    counter = 0
+    Stack = list()
+    new_Path = False # żeby parę razy nie uwzgędniać aktualnie zwracanej ścieżki, używam flagi aby to rozpoznać
+
+    for i in range(index-1,-1,-1):
+        if Arr[i]<Arr[index] and Pull[i] >= maximum-1: # skladam na stos kolejne podciagi, ktore bede odtwarzal       >= <- ponieważ dla aktualnie odtwarzanego podciągu może być to ostatni elemenet, a w innym może być wcześniejszym 
+            Stack.append(i)
+            if new_Path is True:
+                counter += 1 # znalazlo kolejne podciągi konczace sie na tym elemencie
+            new_Path = True
+    Path[0:maximum-1] = [None]* (maximum-1)
+    for k in Stack:
+        Path[maximum-2] = Arr[k]
+        counter += LIS(Arr,Pull,Path,k,maximum-1)
+    return counter
+
 
 def printAllLIS(A):
     Arr = A # na referencji operuje na zmiennej Arr, bo sie do niej przyzwyczailem
@@ -12,7 +36,6 @@ def printAllLIS(A):
             if Arr[j] < Arr[i] and Pull[j]+1 > longest:
                 longest = Pull[j] + 1
         Pull[i] = longest
-        
     maximum = max(Pull)
     counter = 0 # licznik znalezionych podciągów
 
@@ -21,32 +44,11 @@ def printAllLIS(A):
             counter += 1
             Path = [None]*maximum
             Path[maximum-1]= Arr[index] # wrzucam ostatni element jako koniec LIS'a
-            pivot = Arr[index]
-            j = maximum-2 # odtwarzam sciezke, nadpisujac elementy od konca
-            Stack = list() # robie stos aby zapamietac, kolejne podciagi spełniajace warunku
-            for i in range(index-1,-1,-1):
-                if Arr[i]<pivot and Pull[i] == j+1:
-                    pivot = Arr[i]
-                    Path[j] = Arr[i]
-                    j -= 1
-                elif Arr[i]<Arr[index] and Pull[i] == maximum-1: # skladam na stos kolejne podciagi, ktore bede odtwarzal
-                    Stack.append(i)
-                    counter += 1 # znalazlo kolejne podciągi konczace sie na tym elemencie
-            print(Path)
+            counter += LIS(Arr,Pull,Path,index,maximum)
 
-            for k in Stack: # wypakowywuje stos (odtwarzam podciagi)
-                Path = [None]*maximum
-                Path[maximum-1]= Arr[index]
-                Path[maximum-2] = Arr[k]
-                pivot = Arr[k]
-                j = maximum-3
-                for i in range(k-1,-1,-1):
-                    if Arr[i]<pivot and Pull[i] == j+1:
-                        pivot = Arr[i]
-                        Path[j] = Arr[i]
-                        j -= 1 
-                    if j == -1: # gdy stack będzie O(n) to ścieżki będą max: O(2) -> więc nadrobi straty w złożoności nadmieniając O(n^3) -> O(n^2)
-                        break
-                print(Path)
-
+    for i in range(len(Paths_Stack)-1,-1,-1): # zwracam najdłuższe ciągi
+        print(Paths_Stack[i])
     return counter
+
+
+print(printAllLIS(A))
