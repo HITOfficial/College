e1dc7996702e575bc92e0410b7a7f861329256a8
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2021-04-11 14:30:31 +0200

    subsequence sum problem O(n*k)

---
+++ ASD/Lekcja 5 31.03.2021/zad6.py
@@ -0,0 +1,20 @@
+from random import randint
+# podobnie jak w knapsack problemie
+# O(n*k)
+def subsequence_sum(A,k):
+    n = len(A)
+    Arr =[[False for _ in range(k+1)] for _ in range(n)]
+    for i in range(1,n): # szukany podciag dajacy 0 zawsze mozliwy do stworzenia
+        Arr[i][0] = True
+
+    for i in range(1,n):
+        for j in range(1,k+1):
+            Arr[i][j] = Arr[i-1][j]
+            if A[i] <= j:
+                Arr[i][j] = Arr[i][j] or Arr[i-1][j-A[i]]
+    return Arr[n-1][k]
+
+
+
+A = [randint(1,6) for _ in range(6)]
+print(subsequence_sum(A,15))
