08af5cc57c2668481673eb0d4be168355d813fc1
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2021-05-20 18:13:32 +0200

    make_set find, union functions to weighted graphs

---
+++ ASD/Lekcja 12 21.05.2021/make_set_find_union.py
@@ -0,0 +1,34 @@
+class MakeSet:
+    def __init__(self, value, rank=0):
+        self.value = value
+        self.rank = rank
+        self.parent = self
+
+
+def make_set(value):
+    return MakeSet(value)
+
+
+def find(node): # finding first parent from tree, can create not equal two branches tree
+    if node != node.parent:
+        node.parent = find(node.parent)
+    return node.parent
+
+
+def union(tree1,tree2):
+    tree1 = find(tree1)
+    tree2 = find(tree2)
+    if abs(tree1.rank -tree2.rank) > 1:
+        print("can not merge those trees")
+        return
+    if tree1 == tree2: # the same tree
+        return
+    
+    if tree1.rank > tree2.rank: # first tree is higher
+        tree1.rank += 1 # one more level of tree length
+        tree2.parent = tree1
+    elif tree1.rank == tree2.rank: # same level of trees
+            tree2.parent = tree1
+    else: # adding first tree to second one
+        tree2.rank += 1
+        tree1.parent = tree1
