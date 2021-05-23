c3cd56f9806543ed65ff1c1372068fb514883896
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2021-05-23 21:05:35 +0200

    Dijkstra shortest path on PriorityQueue

--- ASD/Lekcja 12 21.05.2021/Dijkstra_shortest_path.py
+++ ASD/Lekcja 12 21.05.2021/Dijkstra_shortest_path.py
@@ -1,5 +1,4 @@
 from queue import PriorityQueue
-import queue
 
 g = [
     [(1,4),(3,3)],
@@ -21,6 +20,15 @@ def relax(costs,start,end,weight,flag): # costs array, start,end, weight of edge
     return False
 
 
+
+def get_path(parent,actual):
+    path = []
+    if parent[actual] is not None:
+        path += [parent[actual]] + get_path(parent,parent[actual])
+    return path
+
+
+
 def Dijkstra_shortest_path(graph,start= None,end=None): # graph, starting vertex
     if start is None:
         start = 0
@@ -30,6 +38,7 @@ def Dijkstra_shortest_path(graph,start= None,end=None): # graph, starting vertex
     n = len(graph)
     counter = len(graph)
     costs = [float("inf")] *n # cost to move from vertex start, to everty vertex
+    parent = [None] * n # in this array I'll memorize path
     p_queue = PriorityQueue()
     p_queue.put((start,start,0))
     flag = True
@@ -37,14 +46,19 @@ def Dijkstra_shortest_path(graph,start= None,end=None): # graph, starting vertex
     while not p_queue.empty() and counter > 0:
         start_vertex, end_vertex, weight = p_queue.get() # edge,weight, of vertex
         if relax(costs, start_vertex, end_vertex, weight,flag) is True: # found better way
+            if flag is False:
+                parent[end_vertex] = start_vertex
             flag = False
             start_vertex = end_vertex
             for edge in graph[end_vertex]: # addding all edges from end vertex
                 end_vertex, weight = edge
                 tpl = start_vertex, end_vertex, weight
                 p_queue.put((tpl))
+                
+    path = get_path(parent,end)
+    path.insert(0,end) # I need to insert last element manualy
 
-    return costs[end]
+    return costs[end], list(reversed(path)), # tuple -> cost, array of vertexes
 
 
-print(Dijkstra_shortest_path(g))
+print(Dijkstra_shortest_path(g,0,4))
