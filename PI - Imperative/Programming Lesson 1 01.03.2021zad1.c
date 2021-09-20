84eae6182397a9ad373718a077939461eb189fbb
HITOfficial <45243378+HITOfficial@users.noreply.github.com>
2021-09-20 13:21:43 +0200

    Delete Programming Lesson 1 01.03.2021zad1.c

--- PI - Imperative/Programming Lesson 1 01.03.2021zad1.c
+++
@@ -1,9 +0,0 @@
-// Wypisz zestawienie temperatur Far-Cel dla f =0,20,40,...,300
-#include <stdio.h>
-
-int main () {
-    for(int i=0; i<= 300; i+=20){
-        int cel = (i+32)/(1.8);
-        printf("%d \n", cel);
-    }
-}
